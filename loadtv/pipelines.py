60ad0312c8f66974e648bbf2fc19f8df1c219a2a
Lineu Felipe <lineufelipe@gmail.com>
2017-04-17 11:55:50 -0300

    Add protobuf and send shows on load

--- loadtv/pipelines.py
+++ loadtv/pipelines.py
@@ -12,6 +12,8 @@ from scrapy.exceptions import DropItem
 from loadtv import settings, items
 import time
 import os
+import tvshow_pb2 as tvshow
+import requests
 os.environ['TZ'] = 'America/Sao_Paulo'
 time.tzset()
 
@@ -34,9 +36,25 @@ class LoadtvPipeline(BaseItemExporter):
             item['start'] = date.replace(hour=int(item['hour'][:2]), minute=int(item['hour'][3:]), second=0, microsecond=0)
             item['end'] = item['start'] + datetime.timedelta(minutes=item['duraction'])
             item['channel_id'] = self.channel_info[item['name']].to_json()
+            buff = self.convert_pb(item, self.channel_info[item['name']]).SerializeToString()
+            r = requests.post("http://localhost:8000/protobuf", data=buff)
+            logging.info(r)
 
             logging.info('Item processed')
             return item
         except Exception as err:
             logging.error('Process Item Error: %s' % err)
             raise DropItem('Process Item Error: %s' % err)
+
+    def convert_pb(self, item, channel):
+        tv_item = tvshow.TvShow()
+        tv_item.channel.group = channel.group
+        tv_item.channel.group_title = channel.group_title
+        tv_item.channel.name = channel.name
+        tv_item.channel.title = channel.title
+        tv_item.desc = item['desc']
+        tv_item.title = item['title']
+        tv_item.start = int(item['start'].timestamp())
+        tv_item.end = int(item['end'].timestamp())
+        tv_item.duraction = int(item['duraction'])
+        return tv_item
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name         = 'loadtv',
-    version      = '1.0.1',
+    version      = '1.1.0',
     packages     = find_packages(),
     entry_points = {'scrapy': ['settings = loadtv.settings']},
 )
